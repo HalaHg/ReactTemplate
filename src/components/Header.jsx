0c0f17b6e119489a767bd089f83b9e017ce80466
Hala.Hage <Hala.Hage@hotmail.com>
2024-02-27 14:44:47 +0200

    add link to header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,15 @@
 import "./Header.css";
+import { Link } from 'react-router-dom';
 export function Header() {
   return (
     <div className="bgHeader">
+      <Link to="">
       <div className="burgerMenu">
         <span></span>
         <span></span>
         <span></span>
       </div>
+      </Link>
       <div className="logo">TEMPLATE</div>
     </div>
   );
