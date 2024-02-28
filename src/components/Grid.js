93770d2b752c2303a862adb7b6ed9fbeb5e47f80
Hala.Hage <Hala.Hage@hotmail.com>
2024-02-28 11:07:53 +0200

    add data

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -1,20 +1,22 @@
 import { MainTitle } from "./MainTitle";
-import { AgGridReact } from 'ag-grid-react';
-import 'ag-grid-community/styles//ag-grid.css';
-import 'ag-grid-community/styles//ag-theme-quartz.css';
-import '../styles/Grid.css'
+import { AgGridReact } from "ag-grid-react";
+import "ag-grid-community/styles//ag-grid.css";
+import "ag-grid-community/styles//ag-theme-quartz.css";
+import "../styles/Grid.css";
+import Breadcrumb from "./Breadcrumb";
 
 export function Grid(props) {
-  const { dataGrid, /*...other*/ } = props;
+  const { dataGrid /*...other*/ } = props;
   return (
     <>
+      <Breadcrumb></Breadcrumb>
       <MainTitle>Virtual Machines</MainTitle>
 
       <div className="ag-theme-quartz">
         <AgGridReact
           columnDefs={dataGrid.columnDefs}
-          rowData={dataGrid.rowData}>
-        </AgGridReact>
+          rowData={dataGrid.rowData}
+        ></AgGridReact>
       </div>
     </>
   );
